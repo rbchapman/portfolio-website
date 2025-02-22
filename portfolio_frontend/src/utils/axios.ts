05d56c1e0f3b2da2730500ca518e6d5bddf7444c
Riley <gp@APPLE-RILEYBC-11.local>
2025-02-22 16:22:10 +0000

    adding debug variable for vite

--- portfolio_frontend/src/utils/axios.ts
+++ portfolio_frontend/src/utils/axios.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_DEBUG 
+  ? 'http://localhost:8000' 
+  : import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   }
 })
-console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);
+
 export default api
