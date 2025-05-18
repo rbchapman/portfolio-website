076ee2ed08bcd81822938d0a9efec3b98292bcdc
Riley Chapman <chapman.riley@gmail.com>
2025-05-18 17:07:16 +0100

    updating store

--- portfolio_frontend/src/router/index.ts
+++ portfolio_frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import { usePhotoStore } from '@/stores/photoStore'
-// import { useUiStore } from '@/stores/uiStore'
+import { usePhotoStore } from '@/stores/photoStore'
+import { useUiStore } from '@/stores/uiStore'
 // import { useCampaignStore } from '@/stores/campaignStore'
 
 const router = createRouter({
@@ -17,7 +17,6 @@ const router = createRouter({
     //   component: () => import('../views/CampaignView.vue')
     // },
     {
-      // Location route (using same component)
       path: '/:location',
       name: 'location',
       component: () => import('../views/HomeView.vue'),
@@ -27,52 +26,52 @@ const router = createRouter({
 })
 
 // Add navigation guard for data loading and transitions
-// router.beforeEach(async (to, from, next) => {
-//   const uiStore = useUiStore()
-//   const photoShootStore = usePhotoStore()
-//   const campaignStore = useCampaignStore()
+router.beforeEach(async (to, from, next) => {
+  const uiStore = useUiStore()
+  const photoShootStore = usePhotoStore()
+  // const campaignStore = useCampaignStore()
   
-//   if (from.name === 'home' || from.name === 'photoshoot') {
-//     uiStore.clearHover()
-//   }
+  if (from.name === 'home') {
+    uiStore.clearHover()
+  }
   
-//   uiStore.setCurrentPage(to.name as string, to.params as Record<string, string>)
-//   try {
-//     // Load data based on route
-//     if (to.name === 'home' && photoShootStore.carouselPhotos.length === 0) {
-//       // await photoShootStore.fetchCarouselPhotos()
-//     }
-//     if (
-//       (to.name === 'home' || to.name === 'photoshoot') &&
-//       photoShootStore.photoShoots.length === 0
-//     ) {
-//       // await photoShootStore.fetchAllCollections()
-//     }
+  uiStore.setCurrentPage(to.name as string, to.params as Record<string, string>)
+  // try {
+  //   // Load data based on route
+  //   if (to.name === 'home' && photoShootStore.carouselPhotos.length === 0) {
+  //     // await photoShootStore.fetchCarouselPhotos()
+  //   }
+  //   if (
+  //     (to.name === 'home' || to.name === 'photoshoot') &&
+  //     photoShootStore.photoShoots.length === 0
+  //   ) {
+  //     // await photoShootStore.fetchAllCollections()
+  //   }
 
-//     if (to.name === 'photography' && photoShootStore.photoShoots.length === 0) {
-//       // await photoShootStore.fetchAllCollections()
-//     }
+  //   if (to.name === 'photography' && photoShootStore.photoShoots.length === 0) {
+  //     // await photoShootStore.fetchAllCollections()
+  //   }
     
-//     // Handle campaigns route
-//     if (to.name === 'campaigns') {
-//       // Fetch campaigns if they haven't been loaded yet
-//       if (campaignStore.campaigns.length === 0) {
-//         await campaignStore.fetchCampaigns()
-//       }
+  //   // Handle campaigns route
+  //   // if (to.name === 'campaigns') {
+  //   //   // Fetch campaigns if they haven't been loaded yet
+  //   //   if (campaignStore.campaigns.length === 0) {
+  //   //     await campaignStore.fetchCampaigns()
+  //   //   }
       
-//       // If no client param is specified and we have campaigns, redirect to first campaign
-//       if (!to.params.client && campaignStore.campaigns.length > 0) {
-//         return next({
-//           path: `/campaigns/${campaignStore.campaigns[0].client}`
-//         })
-//       }
-//     }
+  //   //   // If no client param is specified and we have campaigns, redirect to first campaign
+  //   //   if (!to.params.client && campaignStore.campaigns.length > 0) {
+  //   //     return next({
+  //   //       path: `/campaigns/${campaignStore.campaigns[0].client}`
+  //   //     })
+  //   //   }
+  //   // }
     
-//     next()
-//   } catch (error) {
-//     console.error('Error during route navigation:', error)
-//     next()
-//   }
-// })
+  //   next()
+  // } catch (error) {
+  //   console.error('Error during route navigation:', error)
+  //   next()
+  // }
+})
 
 export default router
