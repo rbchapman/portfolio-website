23b9d4d119ee89570983589d827688a621e4888d
rbchapman <96726657+rbchapman@users.noreply.github.com>
2026-08-09 16:29:48 +0100

    load site copy once at app startup

--- portfolio_frontend/src/main.ts
+++ portfolio_frontend/src/main.ts
@@ -3,8 +3,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { useSiteCopyStore } from '@/stores/siteCopyStore'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
+
+const siteCopyStore = useSiteCopyStore(pinia)
+void siteCopyStore.fetchCopy()
+
 app.mount('#app')
